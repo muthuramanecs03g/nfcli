17fb38f1164193d9dbe41c2dff60bcb0c1e89a90
muthuramanecs03g <muthuramane.cs03g@g2.nctu.edu.tw>
2022-01-21 09:12:39 +0000

    Added interface methods

--- ServerApp/gtp5g/handler.go
+++ ServerApp/gtp5g/handler.go
@@ -7,13 +7,19 @@ import (
 )
 
 func (g *Gtp5gThriftServer) GetPdr(ctx context.Context, iface string, id int32) (_r *UpfGtp5g.GetPdrResponse, _err error) {
-	return nil, nil
+	return &UpfGtp5g.GetPdrResponse{ErrCode: 0,
+		ErrMsg: "Error",
+		Pdr:    nil}, nil
 }
 
 func (g *Gtp5gThriftServer) GetFar(ctx context.Context, iface string, id int32) (_r *UpfGtp5g.GetFarResponse, _err error) {
-	return nil, nil
+	return &UpfGtp5g.GetFarResponse{ErrCode: 0,
+		ErrMsg: "Error",
+		Far:    nil}, nil
 }
 
 func (g *Gtp5gThriftServer) GetQer(ctx context.Context, iface string, id int32) (_r *UpfGtp5g.GetQerResponse, _err error) {
-	return nil, nil
+	return &UpfGtp5g.GetQerResponse{ErrCode: 0,
+		ErrMsg: "Error",
+		Qer:    nil}, nil
 }
